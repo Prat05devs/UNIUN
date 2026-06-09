5fd43f68a19fbeb22b63dc8dcda0b908993c8b83
Pranav Pandey <pranavpandey1998official@gmail.com>
2026-06-09 13:07:37 +0530

    fix: deeplinking

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [{ key: "Content-Type", value: "application/json" }]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
